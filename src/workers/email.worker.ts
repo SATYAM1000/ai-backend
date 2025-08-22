ddf202bb571f195da7c50d1943289cb91d625084
Satyam Kumar <satyamsingh748846@gmail.com>
2025-08-22 16:53:10 +0530

    feat: add presign url

--- src/workers/email.worker.ts
+++ src/workers/email.worker.ts
@@ -1,9 +1,8 @@
 import { Worker } from 'bullmq';
 import { redisClient } from '@/config';
-import { emailServices } from '@/services/email.service';
+import { emailServices, invitationServices } from '@/services';
 import { utils } from '@/utils';
 import { BullMQJobsName } from '@/@types';
-import { invitationServices } from '@/services';
 
 let emailWorker: Worker | null = null;
 
@@ -14,23 +13,20 @@ export const initEmailWorker = () => {
         BullMQJobsName.SEND_WORKSPACE_INVITATION_EMAIL,
         async (job) => {
           if (job.name === BullMQJobsName.SEND_WORKSPACE_INVITATION_EMAIL) {
-            utils.logger('info', `📧 Sending workspace invite email for job ${job?.id}`);
             const { to, templateVariables, invitationId } = job.data;
-            utils.logger('info', `📧 Sending workspace invite email for job ${job?.id} to ${to}`);
-
             try {
               await emailServices.sendWorkspaceInvitationEmail(
                 to,
                 templateVariables.workspaceName,
                 templateVariables,
               );
-              // Only update status if email was sent successfully
+
               await invitationServices.updateEmailSentStatus(invitationId);
               utils.logger('info', `✅ Successfully sent invitation email to ${to}`);
             } catch (error) {
               const errorMessage = error instanceof Error ? error.message : 'Unknown error';
               utils.logger('error', `❌ Failed to send invitation email to ${to}: ${errorMessage}`);
-              throw error; // Re-throw to trigger BullMQ retry mechanism
+              throw error;
             }
           }
         },
